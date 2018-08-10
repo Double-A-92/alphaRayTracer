eec80331ed83b740b27c3a6ffcd0ff8142a87acf
Double-A-92 <amato.amedeo@gmail.com>
2018-08-10 16:27:03 +0200

    add functionality to create image from array of colors
    Ideas of Chapter 1 and 2 implemented in C#

---
+++ Ray Tracing in One Weekend/alphaRayTracer/DirectBitmap.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Numerics;
+using System.Runtime.InteropServices;
+
+namespace alphaRayTracer
+{
+    public class DirectBitmap : IDisposable
+    {
+        public Bitmap Bitmap { get; private set; }
+        public Int32[] Bits { get; private set; }
+        public bool Disposed { get; private set; }
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        protected GCHandle BitsHandle { get; private set; }
+
+        public DirectBitmap(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            Bits = new Int32[width * height];
+            BitsHandle = GCHandle.Alloc(Bits, GCHandleType.Pinned);
+            Bitmap = new Bitmap(width, height, width * 4, PixelFormat.Format32bppPArgb, BitsHandle.AddrOfPinnedObject());
+        }
+
+        public void SetPixel(int x, int y, Color color)
+        {
+            int index = x + (y * Width);
+            int col = color.ToArgb();
+
+            Bits[index] = col;
+        }
+
+        /// <param name="color">Color with RBG values in range [0,1]</param>
+        public void SetPixel(int x, int y, Vector3 color)
+        {
+            color *= byte.MaxValue;
+            SetPixel(x, y, Color.FromArgb((int)color.X, (int)color.Y, (int)color.Z));
+        }
+
+        public Color GetPixel(int x, int y)
+        {
+            int index = x + (y * Width);
+            int col = Bits[index];
+            Color result = Color.FromArgb(col);
+
+            return result;
+        }
+
+        public void Dispose()
+        {
+            if (Disposed) return;
+            Disposed = true;
+            Bitmap.Dispose();
+            BitsHandle.Free();
+        }
+    }
+}
---
+++ Ray Tracing in One Weekend/alphaRayTracer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Numerics;
+
+namespace alphaRayTracer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateTestImage();
+        }
+
+        static void CreateTestImage()
+        {
+            int imageWidth = 2000;
+            int imageHeight = 1000;
+            var image = new DirectBitmap(imageWidth, imageHeight);
+
+            for (var x = 0; x < imageWidth; x++)
+            {
+                for (var y = 0; y < imageHeight; y++)
+                {
+                    var color = new Vector3((float)x / imageWidth, (float)y / imageHeight, 1f/3);
+                    image.SetPixel(x, y, color);
+                }
+            }
+            image.Bitmap.Save("Image.png");
+        }
+    }
+}
