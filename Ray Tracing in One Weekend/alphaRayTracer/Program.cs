a8c5f251ff24f7c1a609e55072ac6aaeeed92aa5
Amedeo Amato <amato.amedeo@gmail.com>
2018-08-12 01:46:14 +0200

    add Ray class and generate Background
    Ideas of Chapter 3 implemented in C#

--- Ray Tracing in One Weekend/alphaRayTracer/Program.cs
+++ Ray Tracing in One Weekend/alphaRayTracer/Program.cs
@@ -13,19 +13,37 @@ namespace alphaRayTracer
 
         static void CreateTestImage()
         {
-            int imageWidth = 2000;
-            int imageHeight = 1000;
+            int imageWidth = 1920;
+            int imageHeight = 1080;
             var image = new DirectBitmap(imageWidth, imageHeight);
+
+            Vector3 origin = Vector3.Zero;
+            Vector3 upperLeftCorner = new Vector3(-imageWidth / 2f, -imageHeight / 2f, -imageHeight);
+            Vector3 horizontal = imageWidth * Vector3.UnitX;
+            Vector3 vertical = imageHeight * Vector3.UnitY;
+
 
             for (var x = 0; x < imageWidth; x++)
             {
                 for (var y = 0; y < imageHeight; y++)
                 {
-                    var color = new Vector3((float)x / imageWidth, (float)y / imageHeight, 1f/3);
+                    float u = (float)x / imageWidth;
+                    float v = (float)y / imageHeight;
+
+                    Ray ray = new Ray(origin, upperLeftCorner + u * horizontal + v * vertical - origin);
+                    var color = GetBackgroundColor(ray);
                     image.SetPixel(x, y, color);
                 }
             }
+
             image.Bitmap.Save("Image.png");
+        }
+
+        static Vector3 GetBackgroundColor(Ray ray)
+        {
+            Vector3 direction = ray.NormalizedDirection;
+            float t = 0.5f * (direction.Y + 1f);
+            return t * Vector3.One + (1f - t) * new Vector3(0.5f, 0.7f, 1);
         }
     }
 }
---
+++ Ray Tracing in One Weekend/alphaRayTracer/Ray.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace alphaRayTracer
+{
+    class Ray
+    {
+        public Vector3 Position { get; private set; }
+        public Vector3 Direction { get; private set; }
+
+        public Ray(Vector3 position, Vector3 direction)
+        {
+            Position = position;
+            Direction = direction;
+        }
+
+        public Vector3 PointAt(float parameter)
+        {
+            return Position + parameter * Direction;
+        }
+
+        public Vector3 NormalizedDirection
+        {
+            get
+            {
+                return Vector3.Normalize(Direction);
+            }
+        }
+    }
+}
